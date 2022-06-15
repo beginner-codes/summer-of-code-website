4753b1eb0d827b703938a46d1981deabe834c2dc
ZechCodes <hi@zech.codes>
2022-06-15 15:39:44 -0400

    Add a select method to the base model

--- soc/database/models/base.py
+++ soc/database/models/base.py
@@ -1,4 +1,7 @@
+from typing import Callable
+
+from sqlalchemy.future import select
 from sqlalchemy.orm import declarative_base as _declarative_base
 
-
 BaseModel = _declarative_base()
+BaseModel.select: Callable[[], select] = classmethod(lambda cls: select(cls))
