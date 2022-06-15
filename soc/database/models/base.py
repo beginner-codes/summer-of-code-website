8ef231b74c2a3f7dc80334fee00e4f291c21030c
ZechCodes <hi@zech.codes>
2022-06-15 19:36:37 -0400

    Clean up the base table model using as_declarative

--- soc/database/models/base.py
+++ soc/database/models/base.py
@@ -1,7 +1,11 @@
-from typing import Callable
-
 from sqlalchemy.future import select
-from sqlalchemy.orm import declarative_base as _declarative_base
+from sqlalchemy.orm import as_declarative as _as_declarative
 
-BaseModel = _declarative_base()
-BaseModel.select: Callable[[], select] = classmethod(lambda cls: select(cls))
+
+@_as_declarative()
+class BaseModel:
+    @classmethod
+    def select(cls) -> select:
+        return select(cls)
+
+    __mapper_args__ = {"eager_defaults": True}
