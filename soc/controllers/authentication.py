f884516ca20c7085a4fce76c2a12e11ddb39e610
ZechCodes <hi@zech.codes>
2022-06-15 16:51:16 -0400

    Add JWT access tokens

--- soc/controllers/authentication.py
+++ soc/controllers/authentication.py
@@ -1,4 +1,5 @@
 import bcrypt
+import jwt
 from bevy import Bevy, Inject
 from bevy.providers import bevy_method
 from pydantic import Field
@@ -8,11 +9,18 @@ from soc.database import Session
 from soc.database.models.users import User
 
 
+class JWTSettings(BaseSettingsModel):
+    private_key: str = Field(default=None, env="SOC_JWT_PRIVATE_KEY")
+    public_key: str = Field(default=None, env="SOC_JWT_PUBLIC_KEY")
+    algorithm: str = Field(default="HS256", env="SOC_JWT_ALGORITHM")
+
+
 class AuthenticationSettings(BaseSettingsModel):
     __config_key__ = "authentication"
 
     salt_rounds: int = Field(default=12, env="SOC_AUTH_SALT_ROUNDS")
-    salt_prefix: str = Field(default="2b", env="SOC_AUTH_SALT_PREFIX")
+    salt_prefix: bytes = Field(default=b"2b", env="SOC_AUTH_SALT_PREFIX")
+    jwt: JWTSettings = Field(default_factory=JWTSettings)
 
 
 class Authentication(Bevy):
@@ -35,11 +43,16 @@ class Authentication(Bevy):
         name: str,
         password: str,
         email: str,
-        session: Session = Inject(),
         settings: AuthenticationSettings = Inject(),
     ):
-        salt = bcrypt.gensalt(settings.salt_rounds, settings.salt_prefix.encode())
+        salt = bcrypt.gensalt(settings.salt_rounds, settings.salt_prefix)
         hashed_password = bcrypt.hashpw(password.encode(), salt).decode()
+        await self._create_user(name, email, hashed_password)
+
+    @bevy_method
+    async def _create_user(
+        self, name: str, email: str, hashed_password: str, session: Session = Inject
+    ):
         async with session.begin():
             session.add(
                 User(
@@ -49,5 +62,10 @@ class Authentication(Bevy):
                 )
             )
 
-    async def create_access_token(self, user: User) -> str:
-        return "ACCESS TOKEN"
+    @bevy_method
+    async def create_access_token(
+        self, user: User, settings: AuthenticationSettings = Inject
+    ) -> str:
+        return jwt.encode(
+            {"user_id": user.id, "username": user.name}, settings.jwt.private_key
+        )
