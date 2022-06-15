a2a452da87779f42e5e987ca180d1f67c4a34986
ZechCodes <hi@zech.codes>
2022-06-15 19:43:04 -0400

    Use correct annotation on the db model select method

--- soc/database/models/base.py
+++ soc/database/models/base.py
@@ -1,11 +1,12 @@
 from sqlalchemy.future import select
 from sqlalchemy.orm import as_declarative as _as_declarative
+from sqlalchemy.sql.expression import Select
 
 
 @_as_declarative()
 class BaseModel:
     @classmethod
-    def select(cls) -> select:
+    def select(cls) -> Select:
         return select(cls)
 
     __mapper_args__ = {"eager_defaults": True}
