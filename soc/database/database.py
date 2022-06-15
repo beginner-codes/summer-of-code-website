3565c19f2b6a084cfe6f38f7f11441657abecc39
Zech Zimmerman <hi@zech.codes>
2022-06-14 21:23:19 -0400

    Apply black

--- soc/database/database.py
+++ soc/database/database.py
@@ -2,9 +2,9 @@ from typing import Type, TypeVar
 
 from bevy import Inject
 from bevy.providers.function_provider import bevy_method
-from bevy.providers.type_provider import TypeProvider
 from bevy.providers.injection_priority_helpers import high_priority
-from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
+from bevy.providers.type_provider import TypeProvider
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from soc.database.config import DatabaseSettings
@@ -24,10 +24,12 @@ class DatabaseProvider(TypeProvider):
         super().__init__()
         self._create_methods = {
             Database: self.create_connection,
-            Session: self.create_session
+            Session: self.create_session,
         }
 
-    def create(self, obj: Type[Database], *args, add: bool = False, **kwargs) -> Database:
+    def create(
+        self, obj: Type[Database], *args, add: bool = False, **kwargs
+    ) -> Database:
         inst = self._create_methods[obj](*args, **kwargs)
         if add:
             self.add(inst, use_as=obj)
@@ -51,12 +53,8 @@ class DatabaseProvider(TypeProvider):
         engine = create_async_engine(settings.uri)
         self.bevy.add(engine, use_as=Database)
         self.bevy.add(
-            sessionmaker(
-                engine,
-                expire_on_commit=False,
-                class_=Session
-            ),
-            use_as=sessionmaker
+            sessionmaker(engine, expire_on_commit=False, class_=Session),
+            use_as=sessionmaker,
         )
         return engine
 
