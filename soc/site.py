5dbb33e4e18600a819f7abfcfcd65ebcd13726f3
ZechCodes <hi@zech.codes>
2022-06-22 19:07:01 -0400

    Correctly get database from the context on startup

--- soc/site.py
+++ soc/site.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from pydantic import Field
 from sqlalchemy.ext.asyncio import AsyncEngine
 
+from soc.admin import admin_app
 from soc.api import api_app
 from soc.config import BaseSettingsModel
 from soc.context import create_context
@@ -11,6 +12,7 @@ from soc.database.models.base import BaseModel
 site = FastAPI()
 
 site.mount("/v1/", api_app)
+site.mount("/admin/", admin_app)
 
 
 class SiteSettings(BaseSettingsModel):
@@ -21,13 +23,14 @@ class SiteSettings(BaseSettingsModel):
 
 @site.on_event("startup")
 async def on_start():
-    context = create_context()
+    context = site.dependency_overrides.get(create_context, create_context)()
     settings = context.get(SiteSettings)
     if settings.dev:
-        database = context.create(AsyncEngine)
-        async with database.begin() as conn:
-            await conn.run_sync(BaseModel.metadata.drop_all)
-            await conn.run_sync(BaseModel.metadata.create_all)
+        database = context.get(AsyncEngine)
+        if not database:
+            database = context.create(AsyncEngine)
+            async with database.begin() as conn:
+                await conn.run_sync(BaseModel.metadata.create_all)
 
 
 @site.get("/", response_class=HTMLResponse)
