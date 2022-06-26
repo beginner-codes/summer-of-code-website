60a626384525276c6f7482c08b4969b2743ee5c7
ZechCodes <hi@zech.codes>
2022-06-26 18:47:02 -0400

    Use templates for the index page

--- soc/site.py
+++ soc/site.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import HTMLResponse
 from pydantic import Field
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from soc.admin import admin_app
 from soc.api import api_app
+from soc.auth_scheme import get_session_from_cookie_no_auth
 from soc.config import BaseSettingsModel
 from soc.context import create_context
-from soc.database.models.base import BaseModel
+from soc.context import inject
+from soc.templates.jinja import Jinja2
 
 site = FastAPI()
 
@@ -32,5 +34,10 @@ async def on_start():
 
 
 @site.get("/", response_class=HTMLResponse)
-async def index():
-    return "<h1>Hello World</h1>"
+async def index(
+    session: dict = Depends(get_session_from_cookie_no_auth),
+    template: Jinja2 = inject(Jinja2),
+):
+    return template(
+        "index.html", username=session.get("username", "<i>NOT LOGGED IN</i>")
+    )
