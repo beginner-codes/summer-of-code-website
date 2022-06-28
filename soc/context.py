d099a36707c0051cb351215ee483731d8f01fbd6
Zech Zimmerman <hi@zech.codes>
2022-06-27 20:21:23 -0400

    Implement bevy routes and add them to new apps

--- soc/context.py
+++ soc/context.py
@@ -1,7 +1,7 @@
 from typing import Callable, Type, TypeVar, overload, ParamSpec
 
 from bevy import Context
-from fastapi import Depends
+from fastapi import Depends, routing, FastAPI
 
 from soc.config.settings_provider import SettingsProvider
 from soc.database.provider import DatabaseProvider
@@ -9,6 +9,32 @@ from soc.database.provider import DatabaseProvider
 T = TypeVar("T")
 R = TypeVar("R")
 P = ParamSpec("P")
+
+
+class BevyRoute(routing.APIRoute):
+    def get_route_handler(self):
+        get_handler = super().get_route_handler
+        handler = None
+
+        async def custom_handler(*args, **kwargs):
+            nonlocal handler
+            if handler is None:
+                if hasattr(self.response_class, "__bevy_context__"):
+                    overrides = self.dependency_overrides_provider.dependency_overrides
+                    context = overrides.get(create_context, create_context)()
+                    self.response_class = context.bind(self.response_class)
+
+                handler = get_handler()
+
+            return await handler(*args, **kwargs)
+
+        return custom_handler
+
+
+def create_app(*args, **kwargs) -> FastAPI:
+    app = FastAPI()
+    app.router.route_class = BevyRoute
+    return app
 
 
 def create_context() -> Context:
