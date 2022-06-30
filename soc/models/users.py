d57f313786e821ccb94971d757bbb8f91a0ad4be
Zech Zimmerman <hi@zech.codes>
2022-06-30 09:55:34 -0400

    Add methods to the user model for managing roles

--- soc/models/users.py
+++ soc/models/users.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 import dataclasses
 import datetime
 
-from bevy import Bevy
+from bevy import Bevy, bevy_method, Inject
 
+import soc.database
 from soc.database.models.users import UserModel
 
 
@@ -17,6 +18,14 @@ class User(Bevy):
     avatar: str | None
     joined: datetime.datetime
     banned: bool
+
+    @bevy_method
+    async def get_roles(self, db: soc.database.Database = Inject) -> list[str]:
+        return await db.users.get_roles(self.id)
+
+    @bevy_method
+    async def set_roles(self, roles: list[str], db: soc.database.Database = Inject):
+        await db.users.set_roles(self.id, roles)
 
     @classmethod
     def from_db_model(cls, model: UserModel) -> User:
