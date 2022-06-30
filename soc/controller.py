f0f906535721143a304ec198be07b302cea928d8
Zech Zimmerman <hi@zech.codes>
2022-06-30 11:28:33 -0400

    Get rid of unused files

--- soc/controller.py
+++
@@ -1,9 +0,0 @@
-from bevy import Bevy, Inject
-from soc.response import Response
-
-
-class Controller(Bevy):
-    response: Response = Inject
-
-    async def run(self):
-        ...
--- soc/response.py
+++
@@ -1,6 +0,0 @@
-class Response:
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        return
