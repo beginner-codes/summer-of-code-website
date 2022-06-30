3b90b46c210539a45accb9b7eef75d0e2c439484
Zech Zimmerman <hi@zech.codes>
2022-06-30 11:22:54 -0400

    Correct session_cookie dependency import

--- soc/site.py
+++ soc/site.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from soc.admin import admin_app
 from soc.api import api_app
 from soc.auth import auth_app
-from soc.authentication_deps import get_session_from_cookie_no_auth
+from soc.authentication_deps import session_cookie
 from soc.config.models.site import SiteSettings
 from soc.context import create_app, create_context
 from soc.templates.response import TemplateResponse
@@ -28,5 +28,5 @@ async def on_start():
 
 
 @site.get("/", response_class=TemplateResponse)
-async def index(session: dict = Depends(get_session_from_cookie_no_auth)):
+async def index(session: dict = Depends(session_cookie)):
     return "index.html", {"username": session.get("username", "<i>NOT LOGGED IN</i>")}
