8ff4e6df9af84fab2e3117da9e88e6ec05a9e800
Zech Zimmerman <hi@zech.codes>
2022-06-30 19:09:58 -0400

    Handle the discord auth state value differently

--- soc/apps/auth.py
+++ soc/apps/auth.py
@@ -1,15 +1,22 @@
 import urllib.parse
+from base64 import urlsafe_b64encode
+from itertools import count
+from random import randbytes
+from time import time
 from typing import Any
 
 import sqlalchemy.exc
-from fastapi import Cookie, HTTPException, Query, Depends
+from fastapi import HTTPException, Query, Depends
 from fastapi.responses import RedirectResponse
 
+from soc.auth_helpers import session_cookie
 from soc.context import create_app, inject
 from soc.controllers.authentication import Authentication, AuthenticationSettings
 from soc.database import Database
 from soc.discord import Discord
 from soc.entities.users import User
+
+counter = count()
 
 auth_app = create_app()
 
@@ -18,9 +25,9 @@ API = "https://discord.com/api/v10"
 
 
 def _verify_state(
-    cookie: str = Cookie(default="", alias="sessionid"), state: str = Query()
+    state: str = Query(), cookie: dict[str, Any] = Depends(session_cookie)
 ):
-    if cookie != state:
+    if cookie["state"] != state:
         raise HTTPException(403, "Invalid state")
 
 
@@ -72,22 +79,27 @@ async def _manage_db_redirect(
 
 @auth_app.get("/discord/login", response_class=RedirectResponse)
 async def discord_login(
-    session_id: str | None = Cookie(default=None, alias="sessionid"),
     auth: Authentication = inject(Authentication),
     settings: AuthenticationSettings = inject(AuthenticationSettings),
 ):
-    if session_id is None:
-        session_id = auth.create_token(type="login")
-
+    state = _create_state()
+    session_id = auth.create_token(type="login", state=state)
     redirect_uri = urllib.parse.quote_plus(settings.discord.redirect_uri)
     response = RedirectResponse(
         f"https://discord.com/api/oauth2/authorize?"
         f"response_type=code&"
         f"client_id={settings.discord.client_id}&"
         f"scope=identify%20email&"
-        f"state={session_id}&"
+        f"state={state}&"
         f"redirect_uri={redirect_uri}&"
         f"prompt=consent"
     )
     response.set_cookie("sessionid", session_id)
     return response
+
+
+def _create_state() -> str:
+    state = bytearray(int(time()).to_bytes(4, "little"))
+    state.extend(next(counter).to_bytes(4, "little"))
+    state.extend(randbytes(34))
+    return urlsafe_b64encode(state).decode()
