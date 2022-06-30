c18261373f77e4100c03833c46dba100481c13e4
Zech Zimmerman <hi@zech.codes>
2022-06-30 16:16:40 -0400

    Populate the template scope with the user data

--- soc/templates/response.py
+++ soc/templates/response.py
@@ -1,8 +1,11 @@
 from typing import Any
 
 from bevy import Bevy, bevy_method, Inject
+from fastapi import Request
 from fastapi.responses import HTMLResponse
 
+from soc.auth_helpers import parse_token
+from soc.config.models.authentication import AuthenticationSettings
 from soc.templates.jinja import Jinja2
 
 
@@ -18,8 +21,20 @@ class TemplateResponse(HTMLResponse, Bevy):
         render_template: Jinja2 = Inject,
     ):
         template_name, template_scope = self._process_data(data)
-        content = render_template(template_name, **template_scope)
+        content = render_template(template_name, **self._populate_scope(), **template_scope)
         super().__init__(content, status_code, headers, media_type, background)
+
+    @bevy_method
+    def _populate_scope(self, request: Request = Inject, auth_settings: AuthenticationSettings = Inject) -> dict[str, Any]:
+        scope = {}
+        session = parse_token(request.cookies.get("sessionid"), auth_settings)
+        if session:
+            scope["user"] = {
+                "username": session.get("username"),
+                "roles": session.get("roles", [])
+            }
+
+        return scope
 
     def _process_data(self, data):
         match data:
