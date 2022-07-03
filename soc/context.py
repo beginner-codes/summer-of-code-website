acda569e60b24e22c926f5b3998069ef6d83046c
Zech Zimmerman <hi@zech.codes>
2022-07-03 16:18:44 -0400

    Load the session into the response context

--- soc/context.py
+++ soc/context.py
@@ -1,12 +1,17 @@
 from collections import ChainMap
 from typing import Callable, Type, TypeVar, overload, ParamSpec
 
+import fastapi
 from bevy import Context
 from fastapi import Depends, routing, FastAPI, Request
 from fastapi.routing import get_request_handler
 
+import soc.auth_helpers
+from soc.config.models.authentication import AuthenticationSettings
 from soc.config.settings_provider import SettingsProvider
+from soc.database import Database
 from soc.database.provider import DatabaseProvider
+from soc.entities.sessions import Session
 
 T = TypeVar("T")
 R = TypeVar("R")
@@ -26,6 +31,7 @@ class BevyRoute(routing.APIRoute):
             overrides_provider = self.dependency_overrides_provider
             overrides = overrides_provider.dependency_overrides
             context: Context = overrides.get(create_context, create_context)().branch()
+            context.add(await self._load_session(request, context), use_as=Session)
             context.add(request, use_as=Request)
             if hasattr(self.response_class, "__bevy_context__"):
                 response_class = context.bind(response_class)
@@ -49,6 +55,15 @@ class BevyRoute(routing.APIRoute):
             return await handler(request, *args, **kwargs)
 
         return custom_handler
+
+    async def _load_session(
+        self, request: fastapi.Request, context: Context
+    ) -> Session | None:
+        auth_settings = context.get(AuthenticationSettings)
+        db = context.get(Database)
+        return await soc.auth_helpers.session_cookie(
+            request.cookies.get("sessionid"), auth_settings, db
+        )
 
 
 def create_app(*args, **kwargs) -> FastAPI:
