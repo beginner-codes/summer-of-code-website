4c18200d3ed0b3aade9ff2c3ea0b597589877def
Zech Zimmerman <hi@zech.codes>
2022-07-03 16:02:52 -0400

    Create migrations for all of the tables

--- soc/database/models/__init__.py
+++ soc/database/models/__init__.py
@@ -1,1 +1,8 @@
+import soc.database.models.challenges
+import soc.database.models.roles
+import soc.database.models.sessions
+import soc.database.models.submission_status
+import soc.database.models.submissions
+import soc.database.models.users
+import soc.database.models.votes
 from soc.database.models.base import BaseModel
