a4151297736aa18c422d130228d4504ba56fc733
Zech Zimmerman <hi@zech.codes>
2022-07-03 16:19:04 -0400

    Populate the template scope using the session

--- soc/templates/response.py
+++ soc/templates/response.py
@@ -1,12 +1,10 @@
 from typing import Any
 
 from bevy import Bevy, bevy_method, Inject
-from fastapi import Request
 from fastapi.responses import HTMLResponse
 
-from soc.auth_helpers import parse_token
-from soc.config.models.authentication import AuthenticationSettings
 from soc.config.models.site import SiteSettings
+from soc.entities.sessions import Session
 from soc.templates.jinja import Jinja2
 
 
@@ -26,10 +24,12 @@ class TemplateResponse(HTMLResponse, Bevy):
         super().__init__(content, status_code, headers, media_type, background)
 
     @bevy_method
-    def _populate_scope(self, request: Request = Inject, auth_settings: AuthenticationSettings = Inject,
-                        site_settings: SiteSettings = Inject) -> dict[str, Any]:
+    def _populate_scope(
+        self,
+        site_settings: SiteSettings = Inject,
+        session: Session = Inject
+    ) -> dict[str, Any]:
         scope = {}
-        session = parse_token(request.cookies.get("sessionid"), auth_settings)
         if session:
             scope["user"] = {
                 "username": session.get("username"),
