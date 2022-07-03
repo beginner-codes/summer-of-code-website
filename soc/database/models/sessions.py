c276d2f940156b1e9570626d5201d0cd6d8a030f
Zech Zimmerman <hi@zech.codes>
2022-07-03 16:13:07 -0400

    Use a big int for session IDs

--- soc/database/models/sessions.py
+++ soc/database/models/sessions.py
@@ -1,11 +1,19 @@
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    ForeignKey,
+    BigInteger,
+    Integer,
+)
 from sqlalchemy.sql import func
+
 from soc.database.models.base import BaseModel
 
 
 class SessionModel(BaseModel):
     __tablename__ = "Sessions"
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
     revoked = Column(Boolean, default=False)
     created = Column(DateTime, server_default=func.now())
     user_id = Column(Integer, ForeignKey("Users.id", ondelete="CASCADE"))
