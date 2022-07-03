46c2ffa768a93594643d5f72b0591707bd6f7f02
Zech Zimmerman <hi@zech.codes>
2022-07-03 16:10:54 -0400

    Get rid of the session fields table

--- soc/database/models/session_fields.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, Unicode
-from soc.database.models.base import BaseModel
-
-
-class SessionFieldModel(BaseModel):
-    __tablename__ = "SessionFields"
-    id = Column(Integer, primary_key=True)
-    data = Column(Unicode(8192), default="")
-    session_id = Column(Integer, ForeignKey("Sessions.id", ondelete="CASCADE"))
