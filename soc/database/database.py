cd0883d92cd97c6baafa0e47216235d55cd975d6
Zech Zimmerman <hi@zech.codes>
2022-07-05 13:37:46 -0400

    Add the challenges interface to the DB interface

--- soc/database/database.py
+++ soc/database/database.py
@@ -1,9 +1,11 @@
 from bevy import Bevy, Inject
 
+from soc.database.challenges import Challenges
 from soc.database.sessions import Sessions
 from soc.database.users import Users
 
 
 class Database(Bevy, inject=Inject.ALL):
+    challenges: Challenges
     sessions: Sessions
     users: Users
