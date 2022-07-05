1b050fd202edcabf1b0817d7c16743ea6baf0f4c
Zech Zimmerman <hi@zech.codes>
2022-07-05 13:37:32 -0400

    Implement basic challenges DB interface

---
+++ soc/database/challenges.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+from typing import Type
+
+import sqlalchemy.exc
+from bevy import Bevy, bevy_method, Inject
+from sqlalchemy import update
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from soc.database.models.challenges import ChallengeModel
+from soc.entities.challenges import Challenge
+from soc.entities.users import User
+
+
+class Challenges(Bevy):
+    def __init__(self):
+        self._challenge_type: Type[Challenge] = self.bevy.bind(Challenge)
+
+    @bevy_method
+    async def create(
+        self,
+        title: str,
+        description: str,
+        start: datetime,
+        end: datetime,
+        user: int | User,
+        db_session: AsyncSession = Inject,
+    ) -> Challenge:
+        model = ChallengeModel(
+            title=title,
+            description=description,
+            start=start.isoformat(),
+            end=end.isoformat(),
+            user_id=user.id if isinstance(user, User) else user,
+        )
+        async with db_session.begin():
+            db_session.add(model)
+
+        return self._challenge_type.from_db_model(model)
+
+    @bevy_method
+    async def get(
+        self, challenge_id: int, db_session: AsyncSession = Inject
+    ) -> Challenge | None:
+        query = select(ChallengeModel).filter_by(id=challenge_id)
+        async with db_session:
+            try:
+                cursor = await db_session.execute(query)
+            except sqlalchemy.exc.OperationalError:
+                return
+            else:
+                model = cursor.scalars().first()
+
+        if not model:
+            return
+
+        return self._challenge_type.from_db_model(model)
+
+    @bevy_method
+    async def update(
+        self, challenge_id: int, db_session: AsyncSession = Inject, **fields
+    ):
+        disallowed_fields = {"created", "id", "user_id"}
+        changed_fields = {
+            field_name: field_value
+            for field_name, field_value in fields.items()
+            if field_name not in disallowed_fields
+        }
+
+        async with db_session.begin():
+            statement = (
+                update(ChallengeModel)
+                .where(ChallengeModel.id == challenge_id)
+                .values(**changed_fields)
+            )
+            await db_session.execute(statement)
+            await db_session.commit()
