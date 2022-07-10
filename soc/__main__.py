59910801e6759b3dbacedf3718080ab3d4bc22b9
Zech Zimmerman <hi@zech.codes>
2022-07-10 18:49:59 -0400

    Make bootstrap configurable

--- soc/__main__.py
+++ soc/__main__.py
@@ -1,4 +1,18 @@
 if __name__ == "__main__":
     import uvicorn
+    import yaml
 
-    uvicorn.run("soc.apps.site:site", host="0.0.0.0", port=8000, log_level="debug")
+    try:
+        file = open("development.config.yaml")
+    except FileNotFoundError:
+        config = {}
+    else:
+        with file:
+            config = yaml.safe_load(file)
+
+    site = config.get("site", {})
+    app = site.get("app", "soc.apps.site:site")
+    host = site.get("host", "0.0.0.0")
+    port = site.get("port", 8000)
+    log_level = site.get("log_level", "debug")
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
