5e76b344e0af178964eee6c2b30f1d3966988179
Zech Zimmerman <hi@zech.codes>
2022-07-10 16:51:34 -0400

    Add an api endpoint for creating a submission

--- soc/apps/site.py
+++ soc/apps/site.py
@@ -12,7 +12,6 @@ from soc.database import Database
 from soc.templates.jinja import Jinja2
 from soc.templates.response import TemplateResponse
 
-
 site = create_app()
 site.mount("/v1/", api_app)
 site.mount("/admin/", admin_app)
@@ -37,9 +36,19 @@ async def index(db: Database = inject(Database)):
     challenge = await db.challenges.get_active()
     return "index.html", {"challenge": await challenge.to_dict() if challenge else None}
 
+
 @site.get("/challenges", response_class=TemplateResponse)
 async def challenges(db: Database = inject(Database)):
     return "challenges.html", {"challenges": await db.challenges.get_all()}
+
+
+@site.get(
+    "/challenges/{challenge_id}/create-submission", response_class=TemplateResponse
+)
+async def challenges(challenge_id: int, db: Database = inject(Database)):
+    return "create_submission.html", {
+        "challenge": await (await db.challenges.get(challenge_id)).to_dict()
+    }
 
 
 @site.get("/logout", response_class=HTMLResponse)
