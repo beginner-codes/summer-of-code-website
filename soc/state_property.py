39e03a2b8e9ad10dea3d93f2a1f059198600a8a9
Zech Zimmerman <hi@zech.codes>
2022-07-10 17:48:56 -0400

    Fix the the issue where state properties were shared

--- soc/state_property.py
+++ soc/state_property.py
@@ -1,34 +1,55 @@
 from __future__ import annotations
 
-from typing import Iterable, Generic, TypeVar, Type, cast
+from typing import Any, cast, Generic, TypeVar, Type
+from weakref import WeakKeyDictionary
 
 T = TypeVar("T")
 
 
 class StateProperty(Generic[T]):
     def __init__(self):
+        self._instances: WeakKeyDictionary[Any, _StateProperty[T]] = WeakKeyDictionary()
+
+    def __get__(self, instance, owner):
+        return self.get(instance)
+
+    def get(self, instance):
+        if instance not in self._instances:
+            self._instances[instance] = _StateProperty[T]()
+
+        return self._instances[instance]
+
+
+class _UnmonitoredProperty(Generic[T]):
+    def __init__(self, state: StateProperty):
+        self._state_property = state
+
+    def __get__(self, instance, owner):
+        state = self._state_property.get(instance)
+        return state.value
+
+    def __set__(self, instance, value):
+        state = self._state_property.get(instance)
+        state.value = value
+
+
+class _MonitoredProperty(_UnmonitoredProperty):
+    def __set__(self, instance, value):
+        super().__set__(instance, value)
+        state = self._state_property.get(instance)
+        state.changed = True
+
+
+class _StateProperty(Generic[T]):
+    def __init__(self):
         self.changed = False
         self.value = None
 
-    def __iter__(self) -> Iterable:
-        return iter(
-            (
-                property(self._getter, self._setter),
-                property(self._getter, self._direct_setter),
-                self,
-            )
-        )
-
-    def _getter(self, _) -> T:
-        return self._value
-
-    def _setter(self, _, value: T):
-        self._direct_setter(_, value)
-        self.changed = True
-
-    def _direct_setter(self, _, value: T):
-        self._value = value
-
 
 def state_property(t: Type[T]) -> tuple[T, T, StateProperty[T]]:
-    return cast(tuple[T, T, StateProperty[T]], StateProperty[T]())
+    state = StateProperty[T]()
+    return (
+        cast(T, _MonitoredProperty(state)),
+        cast(T, _UnmonitoredProperty(state)),
+        state,
+    )
