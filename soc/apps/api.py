7e6f0802445e4b2b662e44fc0699127da88cd6b6
Zech Zimmerman <hi@zech.codes>
2022-07-10 16:54:22 -0400

    Add endpoint to create submission

--- soc/apps/api.py
+++ soc/apps/api.py
@@ -1,9 +1,12 @@
 from fastapi import Depends, Form, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from soc.database import Database
+from pydantic import BaseModel
 
+from soc.auth_helpers import bearer_token, validate_bearer_token
 from soc.context import create_app, inject
 from soc.controllers.authentication import Authentication
+from soc.database import Database
+from soc.entities.sessions import Session
 
 api_app = create_app()
 
@@ -21,6 +24,32 @@ async def register_user(
 @api_app.get("/challenges")
 async def get_challenges(db: Database = inject(Database)):
     return {"challenges": await db.challenges.get_all()}
+
+
+class CreateSubmissionPayload(BaseModel):
+    description: str
+    link: str
+    type: str
+
+
+@api_app.post(
+    "/challenges/{challenge_id}/submissions/create",
+    dependencies=[Depends(validate_bearer_token)],
+)
+async def create_submission(
+    challenge_id: int,
+    submission: CreateSubmissionPayload,
+    session: Session = Depends(bearer_token),
+    db: Database = inject(Database),
+):
+    submission = await db.challenges.create_submission(
+        submission.type,
+        submission.link,
+        submission.description,
+        challenge_id,
+        session.user_id,
+    )
+    return await submission.to_dict()
 
 
 @api_app.get("/challenges/active")
