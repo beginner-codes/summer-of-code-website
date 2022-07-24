8dcb80cde71b236ddfec36185e2b5d427c0df0e4
Zech Zimmerman <hi@zech.codes>
2022-07-24 15:59:58 -0400

    Allow for large emoji names

--- soc/database/models/votes.py
+++ soc/database/models/votes.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, Unicode
 from sqlalchemy.sql import func
+
 from soc.database.models.base import BaseModel
 
 
 class VoteModel(BaseModel):
     __tablename__ = "Votes"
     id = Column(Integer, primary_key=True)
-    emoji = Column(Unicode(12), nullable=False)
+    emoji = Column(Unicode(128), nullable=False)
     created = Column(DateTime, server_default=func.now())
     user_id = Column(Integer, ForeignKey("Users.id"))
     submission = Column(Integer, ForeignKey("Submissions.id", ondelete="CASCADE"))
