6b41b2b7575dc790461e761240256b04bbc0e239
Zech Zimmerman <hi@zech.codes>
2022-07-24 16:09:09 -0400

    Fix GET and HEAD API requests

--- static/scripts.js
+++ static/scripts.js
@@ -1,16 +1,19 @@
 async function apiRequest(method, endpoint, payload){
+    let options = {
+        headers: new Headers(
+            {
+                "Authorization": `Bearer ${sessionToken}`,
+                'content-type': 'application/json'
+            }
+        ),
+        method: method
+    }
+    if(method.toUpperCase() !== "GET" && method.toUpperCase() !== "HEAD")
+        options.body = JSON.stringify(payload ?? {})
+
     const fetched = await fetch(
         endpoint,
-        {
-            headers: new Headers(
-                {
-                    "Authorization": `Bearer ${sessionToken}`, 
-                    'content-type': 'application/json'
-                }
-            ),
-            method: method,
-            body: JSON.stringify(payload ?? {})
-        }
+        options
     )
     return await fetched.json()
 }
