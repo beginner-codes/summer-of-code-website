a9df18ae4aca317f911c87f8d2fcad938c792526
Zech Zimmerman <hi@zech.codes>
2022-07-31 16:40:13 -0400

    Add announcement of approved submissions

--- soc/announcements.py
+++ soc/announcements.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+import re
+
 from bevy import Bevy, Inject
 from bevy.providers.function_provider import bevy_method
+from fastapi import Request
+from httpx import AsyncClient
 
 import soc.entities.submissions as submissions
+from soc.database.settings import Settings
 from soc.events import Events
 
 
@@ -12,6 +17,45 @@ class Announcements(Bevy):
     def __init__(self, events: Events = Inject):
         events.on("submission.status.changed", self.on_submission_status_changed)
 
-    async def on_submission_status_changed(self, submission: submissions.Submission):
-        if submission.status.status == submissions.Status.APPROVED:
-            print("Submission Approved", submission.description)
+    @bevy_method
+    async def on_submission_status_changed(
+        self,
+        submission: submissions.Submission,
+        request: Request,
+        settings: Settings = Inject,
+    ):
+        webhooks = await settings.get("announcement_webhooks")
+        if webhooks and submission.status.status == submissions.Status.APPROVED:
+            async with AsyncClient() as session:
+                user = await submission.created_by
+                description = submission.description.strip()
+                url = f"{request.url_for('index')}#submission-{submission.id}"
+                short_url = self._create_short_link(submission.link)
+                if len(description) > 250:
+                    description = f"{description[:247].rstrip(' !?.:,;)([]{}+-_')}..."
+                resp = await session.post(
+                    webhooks["submission_approved"],
+                    json={
+                        "embeds": [
+                            {
+                                "author": {
+                                    "icon_url": user.avatar,
+                                    "name": user.username,
+                                },
+                                "color": 0xFFE44A,
+                                "description": f"{description}\n\nCheck it out [{short_url}]({url})",
+                            }
+                        ]
+                    },
+                )
+
+                print(
+                    f"Announce submission approval {resp.content=} {resp.status_code=}"
+                )
+
+    def _create_short_link(self, link: str) -> str:
+        short = re.match("(?:https?://)?(.+)", link).group(1)
+        if len(short) > 32:
+            short = f"{short[:32].strip(' /#?')}..."
+
+        return short
