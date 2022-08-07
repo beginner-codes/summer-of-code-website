13eddc2c1b84c0171b480bdadfcd17a29a953dc9
Zech Zimmerman <hi@zech.codes>
2022-08-07 15:25:15 -0400

    Add a logging prefix

--- soc/__main__.py
+++ soc/__main__.py
@@ -1,18 +1,22 @@
-if __name__ == "__main__":
-    import uvicorn
-    import yaml
+import copy
 
-    try:
-        file = open("development.config.yaml")
-    except FileNotFoundError:
-        config = {}
-    else:
-        with file:
-            config = yaml.safe_load(file)
+import uvicorn
+import yaml
 
-    site = config.get("site", {})
-    app = site.get("app", "soc.apps.site:site")
-    host = site.get("host", "0.0.0.0")
-    port = site.get("port", 8000)
-    log_level = site.get("log_level", "debug")
-    uvicorn.run(app, host=host, port=port, log_level=log_level)
+logging_config = copy.deepcopy(uvicorn.config.LOGGING_CONFIG)
+logging_config["formatters"]["default"]["fmt"] = "SOC: %(levelprefix)s %(message)s"
+
+try:
+    file = open("development.config.yaml")
+except FileNotFoundError:
+    config = {}
+else:
+    with file:
+        config = yaml.safe_load(file)
+
+site = config.get("site", {})
+app = site.get("app", "soc.apps.site:site")
+host = site.get("host", "0.0.0.0")
+port = site.get("port", 8000)
+log_level = site.get("log_level", "debug")
+uvicorn.run(app, host=host, port=port, log_level=log_level, log_config=logging_config)
